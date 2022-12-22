f1bad1aa93596d4ff55e405cf11d1228b1b9fab4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-22 14:23:58 +0530

    The useState Hook

--- src/hooks/CounterF.jsx
+++ src/hooks/CounterF.jsx
@@ -2,10 +2,12 @@ import React, { Fragment, useState } from 'react'
 
 const Counter2 = (props) => {
     const [count,setCount] = useState(0);
+    const [name, setName] = useState('');
     return ( 
         <div>
             <Fragment>
-                <div>Count: {count}</div>
+                <input type='text' onChange={e => setName (e.target.value)}/>
+                <div> {name} has clicked {count} times</div>
                 <button onClick={()=> setCount(count +1)}>Increase</button>
             </Fragment>
         </div>
