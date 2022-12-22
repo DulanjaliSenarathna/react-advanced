31e9687b1420437797aa9a225a83087a605bd53a
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-22 16:51:42 +0530

    The useEffect Hook

--- src/hooks/CounterF.jsx
+++ src/hooks/CounterF.jsx
@@ -1,8 +1,16 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 const Counter2 = (props) => {
     const [count,setCount] = useState(0);
     const [name, setName] = useState('');
+
+    useEffect(()=>{
+        document.title = `${name} has clicked ${count} times!`;
+
+        return () =>{
+            console.log('Cleanup');//componentwiiunmout stage. cleanup codes 
+        };
+    },[count]);// [count] means, useEffect function call , only when the count is change
     return ( 
         <div>
             <Fragment>
